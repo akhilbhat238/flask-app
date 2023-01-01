08087f2cf664d4b9cb2b8b1f63f5cdcecba442bb
akhilbhat238 <akhil238@gmail.com>
2023-01-01 17:43:48 +0530

    CORS Dict

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Dean, I have demon blood in me!'
+    my_dict = {'Name':'Mugambo'}
+    return(my_dict)
