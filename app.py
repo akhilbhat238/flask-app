3b30eb473f70f1f19336d126c5db2716de84683d
akhilbhat238 <akhil238@gmail.com>
2023-01-02 18:13:11 +0530

    Identifying Error

--- app.py
+++ app.py
@@ -14,14 +14,20 @@ def create_onedrive_directdownload (onedrive_link):
 @app.route('/')
 def hello_world():
     try:
+        print(1)
         one_drive_link = "https://1drv.ms/x/s!AtIkLugUST4_gbt9Bp4t95oAEJXkbA?e=OVhXnA"
+        print(2)
         one_drive_direct_link = create_onedrive_directdownload(one_drive_link)
+        print(3)
         xls = pd.ExcelFile(one_drive_direct_link)
+        print(4)
         my_dict = {'Mugambo':'Khush Hua'}
+        print(5)
         return(my_dict)
     except Exception as e:
-        my_dict = {'Mugambo':e}
-        return(my_dict)
+        # print(e)
+        # my_dict = {'Mugambo':e}
+        return(e)
 
 
 # @app.route("/spends_per_month",methods=["POST","GET"])
