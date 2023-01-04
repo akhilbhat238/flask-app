c073cd9287ce7d8c6162e745a3af8be3b0688284
akhilbhat238 <akhil238@gmail.com>
2023-01-04 19:23:34 +0530

    Adding all routes

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def create_onedrive_directdownload (onedrive_link):
 one_drive_link = "https://1drv.ms/x/s!AtIkLugUST4_gbt9Bp4t95oAEJXkbA?e=OVhXnA"
 one_drive_direct_link = create_onedrive_directdownload(one_drive_link)
 xls = pd.ExcelFile(one_drive_direct_link)
-my_dict = {}
 
 def return_excel_data(sheet_name):
+    my_dict = {}
     if sheet_name in ['Group_By_Length','Group_By_Type','Group_By_Type_Category']:
         type_df = pd.read_excel(xls, sheet_name)
         for key,value in zip(type_df['Row Labels'],type_df['Sum of Monthly Amount']):
@@ -36,7 +36,6 @@ def return_excel_data(sheet_name):
 
 @app.route('/expenses_by_length')
 def group_by_length():
-    my_dict = {}
     try:
         excel_response = return_excel_data('Group_By_Length')
         return(excel_response)
@@ -45,7 +44,6 @@ def group_by_length():
 
 @app.route('/expenses_by_type')
 def group_by_type():
-    my_dict = {}
     try:
         excel_response = return_excel_data('Group_By_Type')
         return(excel_response)
