a0ee4c36bb2d1da37ac67411ff541c067f0ea0f8
akhilbhat238 <akhil238@gmail.com>
2023-01-28 12:06:12 +0530

    Replacing Excel Pivot with Pandas functions

--- app.py
+++ app.py
@@ -15,30 +15,10 @@ def create_onedrive_directdownload (onedrive_link):
 one_drive_link = "https://1drv.ms/x/s!AtIkLugUST4_gbt9Bp4t95oAEJXkbA?e=OVhXnA"
 one_drive_direct_link = create_onedrive_directdownload(one_drive_link)
 xls = pd.ExcelFile(one_drive_direct_link)
+
 expenses_df = pd.read_excel(xls, 'All_Expenses')
 expenses_df.columns = ['Date', 'Particular', 'Category', 'Spend_Type','Total_Amount', 'Spend_Duration', 'Divide_By','Monthly_Amount']
 expenses_df = expenses_df[['Date', 'Particular', 'Category', 'Spend_Type', 'Total_Amount','Spend_Duration', 'Monthly_Amount']]
-
-def return_excel_data(v_results_df,column_name):
-    my_dict = {}
-    for key,value in zip(v_results_df[column_name],v_results_df['Monthly_Amount']):
-        my_dict[key] = value
-    return(my_dict)
-    # if sheet_name in ['Group_By_Length','Group_By_Type','Group_By_Type_Category']:
-    #     type_df = pd.read_excel(xls, sheet_name)
-    #     for key,value in zip(type_df['Row Labels'],type_df['Sum of Monthly Amount']):
-    #         my_dict[key] = value
-    #     return(my_dict)
-    # elif sheet_name == 'Annual_Personal_Breakdown':
-    #     type_df = pd.read_excel(xls, sheet_name,header=3)
-    #     for key,value in zip(type_df['Row Labels'],type_df['Sum of Monthly Amount']):
-    #         my_dict[key] = value
-    #     return(my_dict)
-    # else:        
-    #     type_df = pd.read_excel(xls, sheet_name,header=2)
-    #     for key,value in zip(type_df['Row Labels'],type_df['Sum of Monthly Amount']):
-    #         my_dict[key] = value
-    #     return(my_dict)    
 
 def return_dict(v_results_df,v_display_column):
     my_dict = {}
@@ -50,7 +30,7 @@ def return_dict(v_results_df,v_display_column):
 @app.route('/expenses_by_length')
 def group_by_length():
     try:
-        results_df = expenses_df.groupby('Spend_Duration')['Monthly_Amount'].sum().reset_index()
+        results_df = expenses_df.groupby('Spend_Duration')['Monthly_Amount'].sum().reset_index().sort_values(by='Monthly_Amount',ascending=False)
         output_dict = return_dict(results_df,'Spend_Duration')
         return(output_dict)
     except Exception as e:
@@ -59,8 +39,7 @@ def group_by_length():
 @app.route('/expenses_by_type')
 def group_by_type():
     try:
-        results_df = expenses_df.groupby('Spend_Type')['Monthly_Amount'].sum().reset_index()
-        # return_excel_data(results_df,'Spend_Type')
+        results_df = expenses_df.groupby('Spend_Type')['Monthly_Amount'].sum().reset_index().sort_values(by='Monthly_Amount',ascending=False)
         output_dict = return_dict(results_df,'Spend_Type')
         return(output_dict)
     except Exception as e:
@@ -69,8 +48,7 @@ def group_by_type():
 @app.route('/expenses_by_category')
 def group_by_category():
     try:
-        results_df = expenses_df.groupby(['Spend_Type','Category'])['Monthly_Amount'].sum().reset_index()
-        # return_excel_data(results_df,'Category')
+        results_df = expenses_df.groupby(['Spend_Type','Category'])['Monthly_Amount'].sum().reset_index().sort_values(by='Monthly_Amount',ascending=False)
         output_dict = return_dict(results_df,'Category')
         return(output_dict)
     except Exception as e:
@@ -79,8 +57,7 @@ def group_by_category():
 @app.route('/annual_expense')
 def annual_expense():
     try:
-        results_df = expenses_df[expenses_df['Spend_Duration']=='A'][['Particular','Monthly_Amount']]
-        # return_excel_data(results_df,'Particular')
+        results_df = expenses_df[expenses_df['Spend_Duration']=='A'][['Particular','Monthly_Amount']].groupby('Particular')['Monthly_Amount'].sum().reset_index().sort_values(by='Monthly_Amount',ascending=False)
         output_dict = return_dict(results_df,'Particular')
         return(output_dict)
     except Exception as e:
@@ -89,8 +66,7 @@ def annual_expense():
 @app.route('/monthly_expense')
 def monthly_expense():
     try:
-        results_df = expenses_df[expenses_df['Spend_Duration']=='M'][['Particular','Monthly_Amount']]
-        # return_excel_data(results_df,'Particular')
+        results_df = expenses_df[expenses_df['Spend_Duration']=='M'][['Particular','Monthly_Amount']].groupby('Particular')['Monthly_Amount'].sum().reset_index().sort_values(by='Monthly_Amount',ascending=False)
         output_dict = return_dict(results_df,'Particular')
         return(output_dict)
     except Exception as e:
@@ -99,8 +75,7 @@ def monthly_expense():
 @app.route('/one_time_expense')
 def one_time_expense():
     try:        
-        results_df = expenses_df[expenses_df['Spend_Duration']=='O'][['Particular','Monthly_Amount']]
-        # return_excel_data(results_df,'Particular')
+        results_df = expenses_df[expenses_df['Spend_Duration']=='O'][['Particular','Monthly_Amount']].groupby('Particular')['Monthly_Amount'].sum().reset_index().sort_values(by='Monthly_Amount',ascending=False)
         output_dict = return_dict(results_df,'Particular')
         return(output_dict)
     except Exception as e:
@@ -109,8 +84,7 @@ def one_time_expense():
 @app.route('/personal_expense_breakdown')
 def personal_expenses():
     try:
-        results_df = expenses_df[(expenses_df['Spend_Duration']=='A')&(expenses_df['Category']=='Personal Purchase')][['Particular','Monthly_Amount']]
-        # return_excel_data(results_df,'Particular')
+        results_df = expenses_df[(expenses_df['Spend_Duration']=='A')&(expenses_df['Category']=='Personal Purchase')][['Particular','Monthly_Amount']].groupby('Particular')['Monthly_Amount'].sum().reset_index().sort_values(by='Monthly_Amount',ascending=False)
         output_dict = return_dict(results_df,'Particular')
         return(output_dict)
     except Exception as e:
